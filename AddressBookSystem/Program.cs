4b41cf78e7f592d0747155869e2b22b0834af3a7
Sachinmahajana <sachinmahajan782@gmail.com>
2023-02-09 23:44:07 +0530

    [Sachin] Adding UC1-CreateContact

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +12,21 @@ namespace AddressBookSystem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to AddressBookSystem Program");
+             Createcontact contacts = new Createcontact();
+            {
+                contacts. First_Name = "Sachin";
+                contacts. Last_Name = "Mahajan";
+                contacts.Address = "Pimpri Chinchwad";
+                contacts.city = "Pune";
+                contacts.state = "Maharastra";
+                contacts.zip = 412105;
+                contacts.PhoneNo = 9370844315;
+                contacts.Email = "sachinmahajan782@gmail.com";
+            }
+            Console.WriteLine(contacts.First_Name + "\n" + contacts.Last_Name + "\n" + contacts.Address + "\n" + contacts.city + "\n"
+            + contacts.state + "\n" + contacts.zip + "\n" + contacts.PhoneNo + "\n" + contacts.Email);
             Console.ReadLine();
+
         }
     }
 }
